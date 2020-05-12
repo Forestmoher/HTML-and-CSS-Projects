7ae517b64952045b75bc738a15c9280e3c06a35c
Forest Moher <fomoher@gmail.com>
2020-05-11 22:08:19 -0700

    Add One page website
    Fixed broken lightbox

--- One-page Website/JS/one_page_website.js
+++ One-page Website/JS/one_page_website.js
@@ -2,6 +2,7 @@ function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
 
+// Close the Modal
 function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
@@ -9,35 +10,30 @@ function closeModal() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-
-
-
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
-    const slides = document.getElementsByClassName("slide");
+    var i;
+    var slides = document.getElementsByClassName("slide");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (var i = 0; i < slides.length; i++) {
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (var i = 0; i < dots.length; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
-}
-
-function currentSlide(n) {
-    showSlides(slideindex = n);
 }
